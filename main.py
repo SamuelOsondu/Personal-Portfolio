b5b85cbba720e6f63b3e06bb635312845107c014
Samuelosondu99@gmail.com <Samuelosondu99@gmail.com>
2022-10-06 22:55:25 +0100

    Edit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, send_file
+import smtplib
+from flask import Flask, render_template, send_file, request
 
 app = Flask(__name__)
 
@@ -12,6 +13,22 @@ def home():
 def download_resume():
     path = "Samuel Osondu - Resume.pdf"
     return send_file(path, as_attachment=True)
+
+
+@app.route('/', methods=["POST"])
+def send_mail():
+    name = request.form['contactName']
+    email = request.form['contactEmail']
+    message = f"Subject: {request.form['contactSubject']} \n\n {request.form['contactMessage']}"
+
+    with smtplib.SMTP_SSL("smtp.gmail.com") as connection:
+        connection.login(user="samuelemailsend@gmail.com", password="fqzvjxtycowglskn")
+        connection.sendmail(
+            from_addr="samuelemailsend@gmail.com",
+            to_addrs="samuelosondu99@gmail.com",
+            msg=message
+        )
+
 
 
 if __name__ == "__main__":
